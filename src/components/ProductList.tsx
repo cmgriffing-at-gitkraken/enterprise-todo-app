0de00df9f38b00c2a5d686a1adebc67e0da278cb
Chris Griffing <chris.griffing@gitkraken.com>
2024-10-18 11:42:44 -0700

    fix: update styling of product list to match updated designs

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -14,9 +14,9 @@ export function ProductList({
     <>
       <div>
         <h3>Widgets</h3>
-        <ul className="mb-4 border border-primary-500 p-2">
+        <ul className="mb-4 border border-[#F59E0B] p-2">
           {widgets.map(({ id, name, description }) => (
-            <li key={id} className="m-2 border border-primary-300 p-1">
+            <li key={id} className="m-2 border border-[#F8BD59] p-1">
               <h4>{name}</h4>
               <p>{description}</p>
             </li>
@@ -25,9 +25,9 @@ export function ProductList({
       </div>
       <div>
         <h3>Gadgets</h3>
-        <ul className="mb-4 border border-primary-500 p-2">
+        <ul className="mb-4 border border-[#F59E0B] p-2">
           {gadgets.map(({ id, name, description }) => (
-            <li key={id} className="m-2 border border-primary-300 p-1">
+            <li key={id} className="m-2 border border-[#F8BD59] p-1">
               <h4>{name}</h4>
               <p>{description}</p>
             </li>
